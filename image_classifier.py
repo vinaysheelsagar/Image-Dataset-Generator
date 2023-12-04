1aa39f780c663c4bd4810cab978ddc4d87426ea8
Vinay Sagar <vine.green12@gmail.com>
2023-12-04 19:35:32 +0530

    added favicon, setup env, bug fix

--- image_classifier.py
+++ image_classifier.py
@@ -6,12 +6,13 @@ from random import choice
 import shutil
 from loguru import logger as log
 from icecream import ic
+import urllib.parse
 
 
 app = Flask(__name__)
 app.config["UNCLASSIFIED_DATASET"] = os.path.join("static", "unclassified_dataset")
 
-valid_extension = 'jpg'
+valid_extensions = ['.jpg', '.png']
 
 
 @app.route("/")
@@ -19,62 +20,77 @@ def home():
     config_file_path = os.path.join(os.getcwd(), "datasets.json")
     with open(config_file_path, 'r') as f:
         config = json.load(f)
-    
+
     dataset_names = config.keys()
     dataset_names = [str(dataset_name) for dataset_name in dataset_names]
+    encoded_dataset_names = [urllib.parse.quote(dataset_name) for dataset_name in dataset_names]
     dataset_count = len(dataset_names)
 
     return render_template(
         "home.html",
         dataset_count=dataset_count,
         dataset_names=dataset_names,
+        encoded_dataset_names=encoded_dataset_names,
     )
 
 
-@app.route("/<dataset_name>")
-def classifier(dataset_name):
+@app.route("/<encoded_dataset_name>")
+def classifier(encoded_dataset_name):
+    decoded_dataset_name = urllib.parse.unquote(encoded_dataset_name)
     config_file_path = os.path.join(os.getcwd(), "datasets.json")
     with open(config_file_path, 'r') as f:
         config = json.load(f)
 
-    all_images_done = False
-    current_image_path = False
     image_name = False
+    current_image_path = False
+    encoded_image_name = False
+    all_images_done = False
 
     unclassified_dataset_dir = Path(Path.cwd(), "static", "unclassified_dataset")
-    valid_images = [file_path.name for file_path in unclassified_dataset_dir.iterdir() if file_path.suffix != valid_extension]
-    # valid_images = unclassified_dataset_dir.glob(f'*.{valid_extension}')
+    valid_images = [file_path.name for file_path in unclassified_dataset_dir.iterdir() if file_path.suffix in valid_extensions]
     ic(len(valid_images))
-    
-    dataset_dir = Path(os.getcwd(), "Dataset", dataset_name,)
-    existing_images_gen = dataset_dir.glob(f'**/*.{valid_extension}')
-    existing_images = [image_path.name for image_path in existing_images_gen]
+
+    dataset_dir = Path(os.getcwd(), "Dataset", decoded_dataset_name,)
+
+    existing_images = list()
+    for ext in valid_extensions:
+        existing_image_gen = dataset_dir.glob(f"**/*{ext}")
+        existing_images.extend([file_path.name for file_path in existing_image_gen])
     ic(len(existing_images))
-    
-    image_names = [image_name for image_name in valid_images if image_name not in existing_images]
-    ic(len(image_names))
+
+    unclassified_image_names = [image_name for image_name in valid_images if image_name not in existing_images]
+    ic(len(unclassified_image_names))
+
     try:
-        image_name = choice(image_names)
+        image_name = choice(unclassified_image_names)
+        encoded_image_name = urllib.parse.quote(image_name)
         current_image_path = os.path.join(app.config["UNCLASSIFIED_DATASET"], f"{image_name}")
     except IndexError:
         all_images_done = True
 
-    categories = config[dataset_name]
+    categories = config[decoded_dataset_name]
+    encoded_categories=[urllib.parse.quote(category) for category in categories]
     categories_count = len(categories)
 
     return render_template(
         "classification.html", 
         all_images_done=all_images_done,
         current_image=current_image_path,
-        image_name=image_name,
-        categories=categories,
         categories_count=categories_count,
-        dataset_name=dataset_name,
+        categories=categories,
+        # script vars
+        image_name=image_name,
+        encoded_categories=encoded_categories,
+        dataset_name=encoded_dataset_name,
+        encoded_image_name=encoded_image_name,
     )
 
 
-@app.route("/<dataset_name>/<cat_id>/<image_name>")
+@app.route("/classify/<dataset_name>/<cat_id>/<image_name>")
 def set_category(dataset_name, cat_id, image_name):
+    dataset_name = urllib.parse.unquote(dataset_name)
+    cat_id = urllib.parse.unquote(cat_id)
+    image_name = urllib.parse.unquote(image_name)
 
     unclassified_dataset_dir = os.path.join(os.getcwd(), "static", "unclassified_dataset")
     dataset_dir = os.path.join(os.getcwd(), "Dataset", dataset_name,)
@@ -86,6 +102,6 @@ def set_category(dataset_name, cat_id, image_name):
     src_image_path = os.path.join(unclassified_dataset_dir, image_name)
     dst_image_path = os.path.join(cat_id_dir, image_name)
 
-    shutil.copyfile(src_image_path, dst_image_path)    
+    shutil.copyfile(src_image_path, dst_image_path)
     
     return "Done!"
---
+++ setup_environment.bat
@@ -0,0 +1,1 @@
+cd /d "%~dp0" & pip install virtualenv & python -m virtualenv venv & venv\Scripts\activate.bat & pip install -r requirements.txt
